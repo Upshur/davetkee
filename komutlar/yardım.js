e52e991025a66bb61f99d885e190a82e55814f1c
Glitch (melon-booming-quiver) <none>
2021-11-25 00:15:33 +0000

    ⭐️🏆 Checkpoint ./komutlar/yardım.js:38223734/416

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -9,7 +9,7 @@ exports.run = async (client, message, args) => {
     .setTitle(`${message.author.username} - Tarafından İstendi`)
       .setColor("GREEN")
   .setAuthor(client.user.username, client.user.avatarURL())
-  .setDescription(`**Davet Komutları: ➕ \n Ana Menü: 💡** \n Selamlar, bu bot PYTHONIC tarafından paylaşıldı. Lütfen abone ol, iyi kullanmalar :D`)
+  .setDescription(`**Davet Komutları: ➕ \n Ana Menü: 💡** \n`)
   .setImage("https://images-ext-1.discordapp.net/external/Bb032GyJs8yCJiUy7tWQ-YnNRPreLuPDo-xp66eOIeU/https/images-ext-2.discordapp.net/external/H1PQhcDr-EaEvwENT8cUxj8S2yonFZl351YbXXH5sGs/https/media.discordapp.net/attachments/697145772801785876/716671769355747348/1.gif")
  var menü = await message.channel.send(yardım)
  const collector = menü.createReactionCollector(filter, { time: 99999 });
@@ -23,8 +23,7 @@ collector.on('collect', (reaction, user) => {
      if(reaction.emoji.name == "➕") {
     const kobscode = new Discord.MessageEmbed()
       .setColor("BLUE")
- .addField("**Davet Komutları**", `\n**!**__rütbe-ekle @rol davet__ = **__Rütbe Ekler__** \n**!**__rütbeler__ = **__Rütbeleri Gösterir 1 Den 10'a Kadar__** \n**!**__rütbe-sıfırla__ = **__Rütbeyi Sıfırlar.__** \n**!**__davetleri-sıfırla__ = **__Davetleri Sıfırlar.__** \n**!**__top__ = **__Lider Tablosunu Gösterir.__** \n**!**__davetlerim__ = **__Davetlerinizi Gösterir.__** \n**!**__bonus-ekle__ = **__Bonus Ekler.__** \n**!**__davet-kanal #kanal__ = **__Davet Kanalını Ayarlar.__** \n**!**__davet-kanal-sıfırla__ = **__Davet Kanalını Sıfırlar.__**`)
-.setImage("https://cdn.discordapp.com/attachments/772883062085386282/775006230082748436/3.png")
+ .addField("**Davet Komutları**", `\n**e+**rütbe-ekle @rol davet = **Rütbe Ekler** \n**e+**rütbeler = **Rütbeleri Gösterir 1 Den 10'a Kadar** \n**!**rütbe-sıfırla = **Rütbeyi Sıfırlar.** \n**!**davetleri-sıfırla = **Davetleri Sıfırlar.** \n**!**top = **Lider Tablosunu Gösterir.** \n**!**davetlerim = **Davetlerinizi Gösterir.**
   .setThumbnail(client.user.avatarURL())
  menü.edit(kobscode)
   }
